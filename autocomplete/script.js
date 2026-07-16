ef6bef417bbf17bcd39d0881439a1af1edf5b0f4
Vijay Raj Soni <vijayrajsoni54@gmail.com>
2026-07-16 20:43:36 +0530

    adding

---
+++ autocomplete/script.js
@@ -0,0 +1,10 @@
+const typed = new Typed(".auto-type",{
+    strings :["Vijay raj soni ","Developer","Engineer","Student"],
+    typeSpeed : 100,
+    backSpeed : 50,
+    loop:true,
+    backDelay : 1000,
+    cursorChar : "|",
+    smartBackspace : true
+})
+
